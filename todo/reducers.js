54c0b9f413d91f3a4ff6409441f4567acaa4abe5
Sergei Komarov <komarovsmail33@gmail.com>
2019-11-13 18:42:10 +0300

    add combine reducers

--- todo/reducers.js
+++ todo/reducers.js
@@ -1,40 +1,59 @@
+const { SHOW_ALL } = VisibilityFilters
+
 import {
   SET_VISIBILITY_FILTER,
   VisibilityFilters, 
   TOGGLE_TODO
 } from './actions'
+import { combineReducers } from 'redux'
 
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
   todos: []
 }
 
-function todoApp(state = initialState, action) {
+//reduce composition
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+  }
+}
+
+function visibilityFilter(state = SHOW_ALL, action) {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
-      return { ...state, visibilityFilter: action.filter }
-    case ADD_TODO:
-      return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
-            text: action.text,
-            completed: false
-          }
-        ]
-      })
-    case TOGGLE_TODO:
-      return Object.assign({}, state, {
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              completed: !todo.completed
-            })
-          }
-          return todo
-        })
-      })
+      return action.filter
     default:
       return state
   }
 }
+
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+})
+
+export default todoApp
+
+// function todoApp(state = {}, action) {
+//   return {
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     todos: todos(state.todos, action)
+//   }
+// }
